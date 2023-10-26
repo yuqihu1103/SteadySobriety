da2251052d10ae936d32c23ad43b873b32954817
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-10-26 11:37:14 -0700

    Create app.js

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import session from "express-session";
+import { connectToDatabase } from "./db/db.js";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+// Start your server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+connectToDatabase();
+export default app;
