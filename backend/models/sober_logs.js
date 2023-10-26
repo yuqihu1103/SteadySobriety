e4707c4f553d1122b49a93be028587ad5cc43682
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-10-26 15:00:54 -0700

    Create sober_logs.js

---
+++ backend/models/sober_logs.js
@@ -0,0 +1,36 @@
+import { getDatabase } from "../db/db.js";
+
+const SoberLogModel = {
+  // CREATE a new sober log
+  async createSoberLog(userId, date) {
+    const db = getDatabase();
+    const soberLogsCollection = db.collection("sober_logs");
+    return soberLogsCollection.insertOne({ userId, date });
+  },
+
+  // READ a user's sober logs
+  async readSoberLogs(userId) {
+    const db = getDatabase();
+    const soberLogsCollection = db.collection("sober_logs");
+    return soberLogsCollection.find({ userId }).toArray();
+  },
+
+  // UPDATE a sober log (assuming by date)
+  async updateSoberLog(userId, oldDate, newDate) {
+    const db = getDatabase();
+    const soberLogsCollection = db.collection("sober_logs");
+    return soberLogsCollection.updateOne(
+      { userId, date: oldDate },
+      { $set: { date: newDate } }
+    );
+  },
+
+  // DELETE a sober log
+  async deleteSoberLog(userId, date) {
+    const db = getDatabase();
+    const soberLogsCollection = db.collection("sober_logs");
+    return soberLogsCollection.deleteOne({ userId, date });
+  },
+};
+
+export default SoberLogModel;
