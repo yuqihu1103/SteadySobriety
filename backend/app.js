74cfd01e47f2edb4082ec869917e6521c09d5ced
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-10-26 13:35:07 -0700

    add middleware and routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,42 @@
 import express from "express";
 import session from "express-session";
-import { connectToDatabase } from "./db/db.js";
 import bodyParser from "body-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import { connectToDatabase } from "./db/db.js";
+import registerRoute from "./routes/register.js";
+import loginRoute from "./routes/login.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+
+//enable session
+app.use(
+  session({
+    secret: "mental-health-monitor",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Middleware to check if the user is authenticated
+function isAuthenticated(req, res, next) {
+  if (req.session && req.session.username) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+// Middleware for parsing JSON request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Use the routes
+app.post("/register", registerRoute);
+app.post("/login", loginRoute);
 
 //test route
 app.get("/test", (req, res) => {
