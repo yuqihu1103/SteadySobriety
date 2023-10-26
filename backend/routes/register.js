7a134a5669000776a80849ceb8f73bb7d1ec767d
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-10-26 13:59:21 -0700

    debug register route

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -8,9 +8,8 @@ const router = express.Router();
 
 // Registration route
 router.post("/register", async (req, res) => {
-  //console.log('POST request to /register received');
+  console.log(req.body);
   const { username, email, password } = req.body;
-
   // server-side validation of username, email, and password
   if (!username || username.length < 3) {
     return res
