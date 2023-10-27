99b27000fb3f20e3cdd5a5e13700ceaa70e790bd
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-10-26 17:48:49 -0700

    try deploy vite app with Github Pages
    try deploy vite app with Github Pages
    
    Update vite.config.js and create .env in vite app with backend service uri
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml
    
    Update deploy-vite.yml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  proxy: {
-    "/api": {
-      target: "https://steady-sobriety-api.onrender.com/",
-      changeOrigin: true,
-      secure: false,
-      ws: true,
-    },
-  },
-  base: process.env.NODE_ENV === "production" ? "/SteadySobriety/" : "/",
 });
