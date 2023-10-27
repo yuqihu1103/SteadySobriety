96ea04da1deb385c21ca6c9437d2b3e8bacc8273
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-10-26 17:03:55 -0700

    add register.jsx and login.jsx

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    credential: "", // This can be either username or email
+    password: "",
+  });
+  const [credentialType, setCredentialType] = useState("Username"); // Default to "Username"
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleCredentialTypeChange = (e) => {
+    setCredentialType(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          credentialType,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 200) {
+        console.log("Login successful:", data);
+      } else {
+        console.error("Login error:", data.error);
+      }
+    } catch (error) {
+      console.error("There was an error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="credentialType">Login with:</label>
+          <select
+            name="credentialType"
+            value={credentialType}
+            onChange={handleCredentialTypeChange}
+          >
+            <option value="Username">Username</option>
+            <option value="Email">Email</option>
+          </select>
+        </div>
+        <div>
+          <label>{credentialType}:</label>
+          <input
+            type="text"
+            name="credential"
+            value={formData.credential}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ frontend/src/Register.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+function Register() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(""); // Clear previous errors
+
+    try {
+      const response = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (response.status === 201) {
+        console.log("Registration successful:", data);
+      } else {
+        setError(data.error);
+      }
+    } catch (error) {
+      setError("An error occurred while registering.");
+      console.error("There was an error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      {error && <div className="error">{error}</div>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+            minLength={3}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+            pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+            minLength={6}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default Register;
