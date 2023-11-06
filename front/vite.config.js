16b3df38c12e9f24e2b6fe312361c5ecd57cf64a
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-11-06 15:46:56 -0800

    modify dependencies

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
