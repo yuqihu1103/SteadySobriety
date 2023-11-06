fece437a64126d9b16867c3f1d5e4d781182adcf
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-11-06 14:51:42 -0800

    add welcome message 2/2

--- front/src/Welcome.jsx
+++ front/src/Welcome.jsx
@@ -1,14 +1,40 @@
-import React from "react";
-import PropTypes from "prop-types"; // This is used for prop type validation
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
 
 const Welcome = ({ loggedInUser }) => {
-  // Check if the loggedInUser exists and display the personalized welcome message
+  const [userStreak, setUserStreak] = useState(0);
+
+  useEffect(() => {
+    // Function to fetch the user's streak from the backend
+    const fetchStreak = async () => {
+      if (loggedInUser) {
+        try {
+          const response = await fetch(`/streak/${loggedInUser}`);
+          const data = await response.json();
+
+          if (response.ok) {
+            setUserStreak(data.streak);
+          } else {
+            throw new Error(data.error || "Error fetching streak.");
+          }
+        } catch (error) {
+          console.error("Fetch streak error:", error);
+        }
+      }
+    };
+
+    fetchStreak();
+  }, [loggedInUser]); // Dependency array ensures this effect runs when loggedInUser changes
+
   return (
     <div>
       {loggedInUser ? (
-        <h1>Welcome to Steady Sobriety, {loggedInUser}!</h1>
+        <>
+          <h1>Welcome to Steady Sobriety, {loggedInUser}!</h1>
+          <p>You have been sober for {userStreak} days!</p>
+        </>
       ) : (
-        <h1>Welcome to Steady Sobriety!</h1> // Default message when no user is logged in
+        <h1>Welcome to Steady Sobriety!</h1>
       )}
     </div>
   );
