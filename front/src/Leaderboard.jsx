7b687829b83d577a18e21285213faf00cb51416f
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-11-06 15:07:46 -0800

    leaderboard: add refresh button, display last update time

--- front/src/Leaderboard.jsx
+++ front/src/Leaderboard.jsx
@@ -2,23 +2,33 @@ import React, { useEffect, useState } from "react";
 
 function Leaderboard() {
   const [leaderboardData, setLeaderboardData] = useState([]);
+  const [lastUpdated, setLastUpdated] = useState(null);
 
-  useEffect(() => {
-    // Fetch leaderboard data when the component mounts
+  const fetchLeaderboardData = () => {
     fetch("/leaderboard")
       .then((response) => response.json())
       .then((data) => {
-        // Set the fetched data to the state
         setLeaderboardData(data);
+        // Set the last updated time
+        setLastUpdated(new Date().toLocaleString());
       })
       .catch((error) => {
         console.error("Error fetching leaderboard data:", error);
       });
+  };
+
+  useEffect(() => {
+    // Fetch leaderboard data when the component mounts
+    fetchLeaderboardData();
   }, []); // The empty dependency array ensures this effect runs once on component mount
 
   return (
     <div>
       <h2>Leaderboard</h2>
+      <div>
+        <span>Last Updated: {lastUpdated}</span>
+        <button onClick={fetchLeaderboardData}>Refresh</button>
+      </div>
       <table>
         <thead>
           <tr>
