8ead1c753d70d3cd1da206bf171cd2a74586473a
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-11-06 16:03:45 -0800

    DrinkingLog: Save Log button only appears after Log Drinking Day clicked

--- front/src/DrinkingLog.jsx
+++ front/src/DrinkingLog.jsx
@@ -5,44 +5,49 @@ import "react-datepicker/dist/react-datepicker.css";
 const DrinkingLog = ({ loggedInUser }) => {
   const [logDate, setLogDate] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
+  const [showSaveButton, setShowSaveButton] = useState(false); // Control the visibility of the Save button
 
-  const handleLogDay = async () => {
-    console.log(loggedInUser);
+  const handleLogDay = () => {
     if (loggedInUser) {
-      try {
-        if (logDate > new Date()) {
-          alert("Cannot pick a future date!");
-          return;
-        }
-        const response = await fetch("/sober-log", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: loggedInUser,
-            date: logDate.toISOString(),
-          }),
-        });
-        const data = await response.json();
-
-        if (response.ok) {
-          alert("Log created successfully!");
-        } else {
-          throw new Error(data.error || "Error logging drinking day.");
-        }
-      } catch (error) {
-        console.error("Error logging drinking day:", error);
-        alert("Failed to log drinking day.");
-      }
+      setShowDatePicker(true);
+      setShowSaveButton(true);
     } else {
       alert("You must be logged in to log a drinking day.");
     }
   };
 
+  const handleSaveLog = async () => {
+    try {
+      if (logDate > new Date()) {
+        alert("Cannot pick a future date!");
+        return;
+      }
+      const response = await fetch("/sober-log", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: loggedInUser,
+          date: logDate.toISOString(),
+        }),
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        alert("Log created successfully!");
+      } else {
+        throw new Error(data.error || "Error logging drinking day.");
+      }
+    } catch (error) {
+      console.error("Error logging drinking day:", error);
+      alert("Failed to log drinking day.");
+    }
+  };
+
   return (
     <div>
-      <button onClick={() => setShowDatePicker(true)}>Log Drinking Day</button>
+      <button onClick={handleLogDay}>Log Drinking Day</button>
       {showDatePicker && (
         <DatePicker
           selected={logDate}
@@ -50,7 +55,7 @@ const DrinkingLog = ({ loggedInUser }) => {
           inline
         />
       )}
-      <button onClick={handleLogDay}>Save Log</button>
+      {showSaveButton && <button onClick={handleSaveLog}>Save Log</button>}
     </div>
   );
 };
