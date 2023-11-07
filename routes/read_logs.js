9398364eba41fac0be90ed7e04a49ee076e8fbc4
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-11-06 16:09:03 -0800

    modify get logs api

--- routes/read_logs.js
+++ routes/read_logs.js
@@ -7,8 +7,8 @@ import UserModel from "../db/models/users.js";
 const router = express.Router();
 
 // Login route
-router.get("/sober-log", async (req, res) => {
-  const { username } = req.body;
+router.get("/sober-log/:username", async (req, res) => {
+  const { username } = req.params;
 
   const user = await UserModel.getUserByUsername(username);
   if (!user) {
