f08d5339b60201612b91d8f268518a64ab430d9c
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-11-07 19:58:38 -0800

    make the intro text to right

--- front/src/IntroScreen.jsx
+++ front/src/IntroScreen.jsx
@@ -6,16 +6,16 @@ const IntroScreen = () => {
     <div className="intro-screen">
       <h1 className="intro-heading">Welcome to Steady Sobriety</h1>
       <div className="content-wrapper">
+        {" "}
+        {/* This should be the flex container */}
         <img src="quitDrinking.jpg" alt="Main" className="main-photo" />
-        <div className="intro-content">
-          <p className="intro-text">
-            SteadySobriety is a web application designed to provide essential
-            tools and support for individuals seeking to overcome alcohol abuse
-            and maintain lasting sobriety. This platform offers a range of
-            features that help users on their journey towards a healthier,
-            alcohol-free life.
-          </p>
-        </div>
+        <p className="intro-text">
+          SteadySobriety is a web application designed to provide essential
+          tools and support for individuals seeking to overcome alcohol abuse
+          and maintain lasting sobriety. This platform offers a range of
+          features that help users on their journey towards a healthier,
+          alcohol-free life.
+        </p>
       </div>
       <footer className="intro-footer">
         <p>Contact the Authors:</p>
