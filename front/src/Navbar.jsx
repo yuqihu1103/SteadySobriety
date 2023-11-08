500b22154abe923ac59965c8ce9fe8acc62905dc
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-11-07 21:01:58 -0800

    add navbar css

--- front/src/Navbar.jsx
+++ front/src/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Navbar.css";
 
 function Navbar({ loggedInUser, handleLogout, setActiveComponent }) {
   // Define functions to change the active component
