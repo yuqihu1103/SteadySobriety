c685eea4015e0c3799c80b7c9982fbda5671398e
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-12-12 14:24:48 -0800

    refactor information icon

--- front/src/components/InformationPopover.jsx
+++ front/src/components/InformationPopover.jsx
@@ -1,40 +1,50 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/InformationPopover.css";
 import PropTypes from "prop-types";
 
-const InformationPopover = ({ content, onClose }) => {
+const InformationPopover = ({ children }) => {
+  const [showPopover, setShowPopover] = useState(false);
   const popoverRef = useRef();
 
   // Event listener to close the popover when clicking outside of it
   useEffect(() => {
     function handleClickOutside(event) {
       if (popoverRef.current && !popoverRef.current.contains(event.target)) {
-        onClose(); // Close the popover
+        setShowPopover(false); // Close the popover
       }
     }
 
-    // Bind the event listener
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      // Unbind the event listener on clean up
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [onClose]);
+  }, []);
 
   return (
-    <div className="information-popover" ref={popoverRef}>
-      <div className="information-content">
-        <h3>Instructions:</h3>
-        <p>{content}</p>
-        <button onClick={onClose}>Close</button>
-      </div>
-    </div>
+    <>
+      <button
+        className="info-icon-button"
+        onClick={() => setShowPopover(!showPopover)}
+        aria-label="Info"
+      >
+        <img src="/information_icon.jpg" alt="Information icon" />
+      </button>
+
+      {showPopover && (
+        <div className="information-popover" ref={popoverRef}>
+          <div className="information-content">
+            <h3>Instructions:</h3>
+            {children} {/* Render the content passed as children */}
+            <button onClick={() => setShowPopover(false)}>Close</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
 InformationPopover.propTypes = {
-  onClose: PropTypes.func.isRequired, // onClose is a required function
-  content: PropTypes.node.isRequired, // content can be any renderable elements
+  children: PropTypes.node.isRequired, // children is now expected to be a React node
 };
 
 export default InformationPopover;
