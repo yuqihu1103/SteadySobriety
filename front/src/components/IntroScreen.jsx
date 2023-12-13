747d2b6a0556b76e8da18c7c7442575694dfd6b7
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-12-12 17:36:27 -0800

    apply color palette

--- front/src/components/IntroScreen.jsx
+++ front/src/components/IntroScreen.jsx
@@ -16,40 +16,38 @@ const IntroScreen = ({ setActiveComponent }) => {
       <div className="intro-heading-container ">
         <h1 className="intro-heading">Welcome to Steady Sobriety</h1>
       </div>
-      <div className="content-wrapper">
-        <div className="row intro-content">
-          {/* Text Content */}
-          <div className="col-lg-6 col-12">
-            <p className="intro-text">
-              SteadySobriety is a web application designed to provide essential
-              tools and support for individuals seeking to overcome alcohol
-              abuse and maintain lasting sobriety.
-            </p>
-            <p className="instruction-text">
-              After registering or logging in with existing credential, you can
-              log you drinking days, view drinking history and current sober
-              streak. Stay sober longer for a chance to be on the leaderboard!
-            </p>
-            <p className="intro-text">
-              <a onClick={onRegisterClicked} className="navigation">
-                {" "}
-                Register
-              </a>{" "}
-              or{" "}
-              <a onClick={onLoginClicked} className="navigation">
-                login
-              </a>{" "}
-              to start tracking your sober time now!
-            </p>
-          </div>
-          {/* Main Photo */}
-          <div className="col-lg-6 col-12">
-            <img
-              src="/main_photo.jpg"
-              alt="Main photo kick alcohol away"
-              className="main-photo img-fluid"
-            />
-          </div>
+      <div className="row intro-content">
+        {/* Text Content */}
+        <div className="col-lg-6 col-12">
+          <p className="intro-text">
+            SteadySobriety is a web application designed to provide essential
+            tools and support for individuals seeking to overcome alcohol abuse
+            and maintain lasting sobriety.
+          </p>
+          <p className="instruction-text">
+            After registering or logging in with existing credential, you can
+            log you drinking days, view drinking history and current sober
+            streak. Stay sober longer for a chance to be on the leaderboard!
+          </p>
+          <p className="intro-text">
+            <a onClick={onRegisterClicked} className="navigation">
+              {" "}
+              Register
+            </a>{" "}
+            or{" "}
+            <a onClick={onLoginClicked} className="navigation">
+              login
+            </a>{" "}
+            to start tracking your sober time now!
+          </p>
+        </div>
+        {/* Main Photo */}
+        <div className="col-lg-6 col-12">
+          <img
+            src="/main_photo.jpg"
+            alt="Main photo kick alcohol away"
+            className="main-photo img-fluid"
+          />
         </div>
       </div>
 
